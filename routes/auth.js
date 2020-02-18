6fd8270ac4b0fe0c23186ef87b025b5e58ba282f
swornimbarahi <sbara002@plattsburgh.edu>
2020-02-17 23:17:00 -0500

    Added auth verify JWT

--- routes/auth.js
+++ routes/auth.js
@@ -49,9 +49,6 @@ router.post("/login", async (req, res) => {
   // create and assign a token
   const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
   res.header('auth-token', token).send(token);
-
-
-  res.send("Logged In")
 })
 
 module.exports = router;
